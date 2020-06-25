7623afa5ceee2d6bc40bec850d0817f9cbf040d6
Bespi123 <62658381+Bespi123@users.noreply.github.com>
2020-06-25 15:56:13 -0500

    BMA400Simulator

--- PlayGround/Brayan/Mqtt-Sim900/Suscriber/main.py
+++ PlayGround/Brayan/Mqtt-Sim900/Suscriber/main.py
@@ -1,27 +1,13 @@
 import paho.mqtt.client as mqtt
-
-
-# This is the Subscriber
-
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    client.subscribe("shovel/terminal/data")
-    #client.subscribe("test")
-
-
-def on_message(client, userdata, msg):
-    mensaje = msg.payload.decode()
-    if msg.payload.decode() == "Fin":
-        print(mensaje)
-        client.disconnect()
-    else:
-        print(mensaje)
-
-
+import time
 client = mqtt.Client()
 client.connect("54.200.157.213", 1883, 60)
-
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.loop_forever()
+# for i in range (0,10):
+with open("noMotion.txt", "r") as file:
+    # This is the Publisher
+    while True:
+        for linea in file.readlines():
+            print(linea)
+            client.publish("shovel/terminal/data", linea)
+            time.sleep(0.2)
+# client.disconnect();
